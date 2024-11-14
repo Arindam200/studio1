c1f5d07db2f0915cb0abff47d3583233bd7b67eb
Arindam200 <arindammajumder2020@gmail.com>
2024-11-15 03:46:54 +0530

    fix mobile layout

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -3,6 +3,7 @@
 // import { useState, useEffect } from 'react'
 import Link from "next/link"
 import  Navbar2  from './acc-navbar'
+import { SideMenuBtn } from './cross'
 
 export default function Navbar() {
   // // const [isScrolled, setIsScrolled] = useState(false)
@@ -23,10 +24,16 @@ export default function Navbar() {
           <img src='https://pbs.twimg.com/profile_images/1816112344445116421/TLDdAxAY_400x400.jpg' className="h-14 w-14 fill-primary text-primary rounded-full" />
           <span className="text-lg font-semibold max-sm:hidden sm:visible">Studio1HQ</span>
         </Link>
-        <Navbar2 />
-        <button className="shadow-[0_4px_14px_0_rgb(0,0,0,10%)] text-lg font-semibold hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2.5 bg-[#fff] text-[#696969] rounded-md transition duration-200 ease-linear">
+        <div className="hidden md:flex">
+          <Navbar2 />
+        </div>
+
+        <button className=" shadow-[0_4px_14px_0_rgb(0,0,0,10%)] hidden md:flex text-sm md:text-lg font-semibold hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2.5 bg-[#fff] text-[#696969] rounded-md transition duration-200 ease-linear">
   Contact Us
 </button>
+        <div className="md:hidden">
+          <SideMenuBtn />
+        </div>
       </div>
     </header>
   )
