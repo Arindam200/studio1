f7f4e873db3708c45d05184b40d28b904f5e0cf6
Arindam200 <arindammajumder2020@gmail.com>
2024-11-15 03:55:13 +0530

    mobile improvements

--- components/ui/acc-navbar.tsx
+++ components/ui/acc-navbar.tsx
@@ -8,21 +8,21 @@ import { cn } from "@/lib/utils";
 export default function Navbar2({ className }: { className?: string }) {
   return (
     <div
-      className={cn("fixed top-4 inset-x-0 max-w-2xl mx-auto z-50 rounded-md py-2 gap-6", className)}
+      className={cn("fixed top-4 inset-x-0 max-w-2xl mx-auto z-50 rounded-md py-2 gap-6 max-sm:mx-1 text-lg max-sm:text-sm", className)}
     >
       <Menu >
         
         <Link href="/work" >
-          <p className="text-lg font-medium text-white">Work</p>
+          <p className=" font-medium text-white">Work</p>
         </Link>
         <Link href="/services" >
-          <p className="text-lg font-medium text-white">Services</p>
+          <p className=" font-medium text-white">Services</p>
         </Link>
         <Link href="/process" >
-          <p className="text-lg font-medium text-white">Process</p>
+          <p className=" font-medium text-white">Process</p>
         </Link>
         <Link href="/testimonial" >
-          <p className="text-lg font-medium text-white">Testimonial</p>
+          <p className=" font-medium text-white">Testimonial</p>
         </Link>
       </Menu>
     </div>
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,39 +1,49 @@
 'use client'
 
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import Link from "next/link"
 import  Navbar2  from './acc-navbar'
-import { SideMenuBtn } from './cross'
+// import { SideMenuBtn } from './cross'
+// import { is } from '@react-three/fiber/dist/declarations/src/core/utils'
 
 export default function Navbar() {
-  // // const [isScrolled, setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false)
 
-  // useEffect(() => {
-  //   // const handleScroll = () => {
-  //   //   setIsScrolled(window.scrollY > 20)
-  //   // }
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 20)
+    }
 
-  //   window.addEventListener('scroll', handleScroll)
-  //   return () => window.removeEventListener('scroll', handleScroll)
-  // }, [])
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   return (
     <header className="fixed top-2 z-50 w-full">
       <div className="container flex h-20 max-w-screen-2xl items-center justify-between">
-        <Link href="/" className="flex items-center space-x-2">
+        {!isScrolled && (
+          <Link href="/" className="flex items-center space-x-2">
           <img src='https://pbs.twimg.com/profile_images/1816112344445116421/TLDdAxAY_400x400.jpg' className="h-14 w-14 fill-primary text-primary rounded-full" />
           <span className="text-lg font-semibold max-sm:hidden sm:visible">Studio1HQ</span>
-        </Link>
-        <div className="hidden md:flex">
-          <Navbar2 />
-        </div>
+        </Link>)
+          }
+        
 
-        <button className=" shadow-[0_4px_14px_0_rgb(0,0,0,10%)] hidden md:flex text-sm md:text-lg font-semibold hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2.5 bg-[#fff] text-[#696969] rounded-md transition duration-200 ease-linear">
-  Contact Us
-</button>
-        <div className="md:hidden">
+{
+  isScrolled && (
+    <Navbar2 />
+  )
+}
+
+        
+        {!isScrolled && (
+          <button className=" shadow-[0_4px_14px_0_rgb(0,0,0,10%)] flex text-sm md:text-lg font-semibold hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2.5 bg-[#fff] text-[#696969] rounded-md transition duration-200 ease-linear">
+          Contact Us
+        </button>)
+          }
+        {/* <div className="md:hidden">
           <SideMenuBtn />
-        </div>
+        </div> */}
       </div>
     </header>
   )
