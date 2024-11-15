a3f048a64bacc4692b3c0fcdb055d8702c9b73df
Arindam200 <arindammajumder2020@gmail.com>
2024-11-15 10:42:21 +0530

    overflow fix

--- components/ui/background.tsx
+++ components/ui/background.tsx
@@ -8,7 +8,7 @@ import Navbar from "./navbar";
 
 export function GridBackgroundDemo() {
   return (
-    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
+    <div className="h-[50rem] overflow-x-hidden w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
       <Navbar />
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -19,25 +19,25 @@ export default function Navbar() {
   }, [])
 
   return (
-    <header className="fixed top-2 z-50 w-full">
+    <header className="fixed top-2 z-50 w-full overflow-x-hidden">
       <div className="container flex h-20 max-w-screen-2xl items-center justify-between">
         {!isScrolled && (
           <Link href="/" className="flex items-center space-x-2">
-          <img src='https://pbs.twimg.com/profile_images/1816112344445116421/TLDdAxAY_400x400.jpg' className="h-14 w-14 fill-primary text-primary rounded-full" />
+          <img src='https://pbs.twimg.com/profile_images/1816112344445116421/TLDdAxAY_400x400.jpg' className="h-14 w-14 max-md:h-10 max-md:w-10 fill-primary text-primary rounded-full" />
           <span className="text-lg font-semibold max-sm:hidden sm:visible">Studio1HQ</span>
         </Link>)
           }
-        
-
-{
-  isScrolled && (
-    <Navbar2 />
-  )
-}
-
-        
+          
+          <div className="hidden md:flex">
+            <Navbar2 />
+          </div>
+        {
+          isScrolled && (
+            <Navbar2 />
+          )
+        }        
         {!isScrolled && (
-          <button className=" shadow-[0_4px_14px_0_rgb(0,0,0,10%)] flex text-sm md:text-lg font-semibold hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2.5 bg-[#fff] text-[#696969] rounded-md transition duration-200 ease-linear">
+          <button className=" shadow-[0_4px_14px_0_rgb(0,0,0,10%)] flex text-lg  max-md:text-sm font-semibold max-md:font-light hover:shadow-[0_6px_20px_rgba(93,93,93,23%)] px-8 py-2.5 max-md:px-4 max-md:py-1.5 bg-[#fff] text-[#696969] rounded-md transition duration-200 ease-linear">
           Contact Us
         </button>)
           }
