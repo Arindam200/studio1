ea854fe6aa420ef8f862c5837e8d5a651fb3aeb4
Arindam200 <arindammajumder2020@gmail.com>
2024-11-17 00:31:49 +0530

    blogs hero page

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+"use client";
 
-function Blogs() {
+import React from "react";
+import { Spotlight } from "@/components/ui/spotlight";
+import { NumberTicker } from "@/components/ui/counter";
+import Navbar from "@/components/sections/navbar";
+// import type { Variants } from 'framer-motion';
+// import { motion, useAnimation } from 'framer-motion';
+
+// const gVariants: Variants = {
+//   normal: { rotate: 0 },
+//   animate: { rotate: 180 },
+// };
+
+
+export default function Page() {
+
+  // const controls = useAnimation();  
   return (
-    <div>Blogs</div>
-  )
-}
+    <div className="h-[50rem] overflow-x-hidden w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center cursor-all-scroll">
+      <Navbar />
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-export default Blogs
+      <Spotlight />
+      <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+  <span className="absolute inset-0 overflow-hidden rounded-full">
+    <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+  </span>
+  <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+    <span>
+      Don't Miss this Chance!
+    </span>
+    <svg
+      fill="none"
+      height="16"
+      viewBox="0 0 24 24"
+      width="16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.75 8.75L14.25 12L10.75 15.25"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      />
+    </svg>
+  </div>
+  <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
+</button>
+      <div className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 pt-8 py-4 text-center">
+       <span className="font-bold text-transparent bg-clip-text bg-gradient-to-b from-orange-400 via-orange-500 to-orange-600 from"> Blog as a Service</span>
+
+      <p className="text-lg max-sm:px-2.5 sm:text-2xl font-semibold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-4 text-center">
+      Empowering software companies with high-quality, tech-driven content that drives growth.
+      </p>
+      <div className="flex gap-6 justify-center text-3xl mt-4">
+        <div className="">
+        <NumberTicker value={50} /> + Blogs Written
+        </div>
+        <div className="">
+        <NumberTicker value={100} /> + Happy Customers
+        </div>
+        <div className="">
+        <NumberTicker value={100} /> % Satisfaction
+          </div>
+      </div>
+      </div> 
+    </div>
+  );
+}
