74872e50681de2ea7214a8f7b70e31dac517c8f9
Mr. Ånand <73425223+Astrodevil@users.noreply.github.com>
2024-11-26 21:51:40 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
+    // Disable remotePatterns and allow fetching images from any host
+    unoptimized: true,
   },
 };
 
