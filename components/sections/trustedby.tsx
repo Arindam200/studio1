effa16e9b3686b84e121580a6183047500fed797
Arindam200 <arindammajumder2020@gmail.com>
2024-11-30 13:53:52 +0530

    fix: trustedby section

--- components/sections/trustedby.tsx
+++ components/sections/trustedby.tsx
@@ -8,12 +8,16 @@ function Trustedby() {
   return (
     <div className="w-full overflow-hidden py-6 sm:py-8 md:py-10">
       <Marquee>
-        {TrustedbyLogo.map((Logo, index) => (
+        {TrustedbyLogo.map((Image,index) => (
           <div
             key={index}
             className="relative mx-6 sm:mx-8 md:mx-[4rem] flex items-center justify-center"
           >
-            <Logo />
+            <img
+              src={Image.image}
+              alt={Image.alt}
+              className={Image.className}
+            />
           </div>
         ))}
       </Marquee>
