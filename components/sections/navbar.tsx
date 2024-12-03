539c745a8280cc5db4b879024c154a77d529c4a5
Arindam200 <arindammajumder2020@gmail.com>
2024-12-03 19:10:36 +0530

    fix: button issue

--- components/sections/navbar.tsx
+++ components/sections/navbar.tsx
@@ -58,7 +58,7 @@ export default function Navbar() {
         {!isScrolled && (
           <a
             href="mailto:studioone.tech@gmail.com"
-            className="shadow-[0_4px_14px_0_rgb(0,0,0,10%)] flex text-lg max-md:text-sm font-semibold px-8 py-2.5 max-md:px-4 max-md:py-1.5 bg-[#fff] text-orange-500 rounded-md transition duration-200 ease-linear hover:shadow-[0_6px_20px_rgba(93,93,93,23%)]"
+            className="shadow-[0_4px_14px_0_rgb(0,0,0,10%)] flex text-lg max-md:text-sm font-bold px-8 py-2.5 max-md:px-4 max-md:py-1.5 bg-orange-500 text-white rounded-md transition duration-200 ease-linear hover:shadow-[0_6px_20px_rgba(93,93,93,23%)]"
           >
             Contact Us
           </a>
